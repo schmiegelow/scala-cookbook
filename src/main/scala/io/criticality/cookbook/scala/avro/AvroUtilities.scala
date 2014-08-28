2353a65a4e4d1d7ac038f8f36260fe82f2928533
Erik Schmiegelow <e.schmiegelow@xplosion.de>
2014-08-28 09:40:26 +0200

    added avro utilities

---
+++ src/main/scala/io/criticality/cookbook/scala/avro/AvroUtilities.scala
@@ -0,0 +1,33 @@
+package io.criticality.cookbook.scala.avro
+
+import java.io.File
+import org.apache.avro.Schema
+import org.apache.avro.file.DataFileWriter
+import org.apache.avro.specific.SpecificDatumWriter
+import org.apache.avro.specific.SpecificRecordBase
+import org.apache.avro.specific.SpecificDatumReader
+import org.apache.avro.file.DataFileReader
+
+class AvroUtilities {
+
+  def writeAvro[A <: SpecificRecordBase](records: Seq[A], schema: Schema, path: String) {
+    val writer = new DataFileWriter(new SpecificDatumWriter[A](schema))
+    writer.create(schema, new File(path))
+    records.foreach {
+      record => writer.append(record);
+    }
+    writer.flush
+    writer.close
+  }
+
+  def readAvro[A <: SpecificRecordBase](path: String, schema: Schema): Seq[A] = {
+    val datum = new SpecificDatumReader[A](schema);
+    val reader = new DataFileReader[A](new File(path), datum);
+    val results = Seq.newBuilder[A]
+    while (reader.hasNext) {
+      results += reader.next()
+    }
+    results.result
+  }
+
+}
