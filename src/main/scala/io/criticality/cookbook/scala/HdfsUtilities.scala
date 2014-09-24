48d8c8765a562ac6c8a5659ce67955055f16680c
Erik Schmiegelow <e.schmiegelow@xplosion.de>
2014-09-24 10:44:02 +0200

    added UGI and getOutput method to hdfs utils

--- src/main/scala/io/criticality/cookbook/scala/HdfsUtilities.scala
+++ src/main/scala/io/criticality/cookbook/scala/HdfsUtilities.scala
@@ -1,13 +1,19 @@
 package io.criticality.cookbook.scala
-import java.io.File
+
 import org.apache.hadoop.conf._
 import org.apache.hadoop.fs._
-import scala.io.Source
+import org.apache.hadoop.security.UserGroupInformation
+
+import java.io.File
 import java.io.BufferedWriter
 import java.io.BufferedOutputStream
 import java.io.BufferedReader
 import java.io.FileWriter
 import java.io.InputStreamReader
+import java.security.PrivilegedExceptionAction
+
+import scala.io.Source
+import scala.collection.JavaConversions._
 
 /**
  * A set of utility classes for HDFS file system access
@@ -23,16 +29,23 @@ object HdfsUtilities {
 
   private val fileSystem = FileSystem.get(conf)
 
-  def saveFile(filepath: String): Unit = {
+  def saveFile(filepath: String, remote: String): String = {
     val file = new File(filepath)
-    val out = new BufferedOutputStream(fileSystem.create(new Path(file.getName)))
-    for (line <- Source.fromFile(file).getLines()) {
-      var b = line.getBytes();
-      while (b.length > 0) {
-        out.write(b)
+    val ugi = UserGroupInformation.createRemoteUser(getHdfsUser)
+    transferCredentials(UserGroupInformation.getCurrentUser(), ugi)
+    ugi.doAs(new PrivilegedExceptionAction[Unit] {
+      def run: Unit = {
+        val out = new BufferedOutputStream(fileSystem.create(new Path(remote)))
+        for (line <- Source.fromFile(file).getLines()) {
+          var b = line.getBytes();
+          while (b.length > 0) {
+            out.write(b)
+          }
+        }
+        out.close()
       }
-    }
-    out.close()
+    })
+    remote
   }
 
   def exists(filename: String): Boolean = {
@@ -42,27 +55,90 @@ object HdfsUtilities {
 
   def removeFile(filename: String): Boolean = {
     val path = new Path(filename)
-    fileSystem.delete(path, true)
+    var sucess = false
+    val ugi = UserGroupInformation.createRemoteUser(getHdfsUser)
+    transferCredentials(UserGroupInformation.getCurrentUser(), ugi)
+    ugi.doAs(new PrivilegedExceptionAction[Unit] {
+      def run: Unit = {
+        sucess = fileSystem.delete(path, true)
+      }
+    })
+    sucess
   }
 
   def getFile(remote: String, local: String): String = {
     val path = new Path(remote)
-    val rd = new BufferedReader(new InputStreamReader(fileSystem.open(path)))
-    val wt = new BufferedWriter(new FileWriter(local))
-    var line: String = rd.readLine
-    while (line != null) {
-      wt.write(line);
-      line = rd.readLine
-    }
-    rd.close
-    wt.close
+    val ugi = UserGroupInformation.createRemoteUser(getHdfsUser)
+    transferCredentials(UserGroupInformation.getCurrentUser(), ugi)
+    ugi.doAs(new PrivilegedExceptionAction[Unit] {
+      def run: Unit = {
+        val rd = new BufferedReader(new InputStreamReader(fileSystem.open(path)))
+        val wt = new BufferedWriter(new FileWriter(local))
+        var line: String = rd.readLine
+        while (line != null) {
+          wt.write(line);
+          line = rd.readLine
+        }
+        rd.close
+        wt.close
+      }
+    })
     local
   }
 
-  def createFolder(folderPath: String): Unit = {
+  def createFolder(folderPath: String): Boolean = {
     val path = new Path(folderPath)
-    if (!fileSystem.exists(path)) {
-      fileSystem.mkdirs(path)
+    var sucess = false
+    val ugi = UserGroupInformation.createRemoteUser(getHdfsUser)
+    transferCredentials(UserGroupInformation.getCurrentUser(), ugi)
+    ugi.doAs(new PrivilegedExceptionAction[Unit] {
+      def run: Unit = {
+        if (!fileSystem.exists(path)) {
+          sucess = fileSystem.mkdirs(path)
+        }
+      }
+    })
+    sucess
+  }
+
+  /**
+   * retrieves the entire output of a map reduce operation designated by the @param remote folder
+   */
+  def getOutput(remote: String, local: String): String = {
+
+    val status = List.fromArray(fileSystem.listStatus(new Path(remote)))
+    System.out.println("Found " + status.size + " in " + remote);
+    val wt = new BufferedWriter(new FileWriter(local))
+    val ugi = UserGroupInformation.createRemoteUser(getHdfsUser)
+    transferCredentials(UserGroupInformation.getCurrentUser(), ugi)
+    ugi.doAs(new PrivilegedExceptionAction[Unit] {
+      def run: Unit = {
+        status.foreach(item => {
+          if (item.getPath().toString().contains("part")) {
+            val br = new BufferedReader(new InputStreamReader(fileSystem.open(item.getPath())))
+            var line: String = br.readLine
+            while (line != null) {
+              System.out.println(line);
+              wt.append(line).append("\n");
+              line = br.readLine()
+            }
+            br.close
+          }
+        })
+        wt.close
+      }
+    })
+    local
+
+  }
+
+  def getHdfsUser(): String = {
+    conf.get("hadoop.job.ugi");
+  }
+
+  def transferCredentials(source: UserGroupInformation, dest: UserGroupInformation) {
+    for (token <- source.getTokens()) {
+      dest.addToken(token)
     }
   }
 }
