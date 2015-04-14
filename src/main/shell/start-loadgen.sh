f5ac76405803a5b18454749c2354973456ebef42
Erik Schmiegelow <erik.schmiegelow@hamburg.de>
2015-04-14 18:55:31 +0200

    added LoadRunner

---
+++ src/main/shell/start-loadgen.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+echo "Executing in $dir with args $1 $2 $3"
+
+echo "Running java -cp $dir/scala-cookbook-0.0.1-SNAPSHOT-shaded.jar io.criticality.app.LoadGenRunner \
+-f $1 -r $2 -d $3"
+
+java -cp $dir/scala-cookbook-0.0.1-SNAPSHOT-shaded.jar io.criticality.app.LoadGenRunner  \
+-f $1 -r $2 -d $3
