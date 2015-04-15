d10965bc3eecb9ccdd7b769870d6bfb14453cc97
Erik Schmiegelow <erik.schmiegelow@hamburg.de>
2015-04-15 14:30:09 +0200

    some script fixes

--- src/main/shell/start-loadgen.sh
+++ src/main/shell/start-loadgen.sh
@@ -3,7 +3,6 @@
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 echo "Executing in $dir with args $1 $2 $3"
-
 echo "Running java -cp $dir/scala-cookbook-0.0.1-SNAPSHOT-shaded.jar io.criticality.app.LoadGenRunner \
 -f $1 -r $2 -d $3"
 
